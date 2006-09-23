a80c968181ae15e19c867ec528f29d54521e6c55
bprodoehl <bprodoehl@localhost>
2006-09-23 16:47:15 +0000

    Adding simulated unmanned vehicle, featuring Global Pose Sensor, Velocity State Sensor, Primitive Driver, and Waypoint Driver components.

---
+++ openjaus/trunk/Components/examples/vehicle/src/gpos.c
@@ -0,0 +1,472 @@
+// File:		gpos.c 
+// Version:		0.4 alpha
+// Written by:	Tom Galluzzo (galluzzt@ufl.edu) and Danny Kent (kentd@ufl.edu)
+// Date:		07/01/2005
+// Description:	This file contains the skeleton C code for implementing a JAUS component in a Linux environment
+//				This code is designed to work with the node manager and JAUS library software written by CIMAR
+
+#include <cimar.h>
+#include <cimar/jaus.h>			// JAUS message set (USER: JAUS libraries must be installed first)
+#include <cimar/nodeManager.h>	// Node managment functions for sending and receiving JAUS messages (USER: Node Manager must be installed)
+#include <pthread.h>			// Multi-threading functions (standard to unix)
+#include <stdlib.h>	
+#include <unistd.h>				// Unix standard functions
+#include <string.h>
+// USER: Add include files here as appropriate
+
+#include "gpos.h"	// USER: Implement and rename this header file. Include prototypes for all public functions contained in this file.
+#include "vehicleSim.h"
+
+// Private function prototypes
+void *gposThread(void *);
+void gposProcessMessage(JausMessage);
+void gposStartupState(void);
+void gposInitState(void);
+void gposStandbyState(void);
+void gposReadyState(void);
+void gposEmergencyState(void);
+void gposFailureState(void);
+void gposShutdownState(void);
+void gposAllState(void);
+
+static JausAddressStruct gposAddress =	{ { 0, JAUS_GLOBAL_POSE_SENSOR, 0, 0} }; 
+static JausAddressStruct gposControllerAddress =	{ { 0, 0, 0, 0} }; 
+static JausComponentStruct gposStruct =	{	"gpos",
+											&gposAddress,
+											JAUS_SHUTDOWN_STATE,
+											0,
+											NULL,
+											NULL,
+											{
+												&gposControllerAddress,
+												JAUS_UNDEFINED_STATE,
+												0,
+												JAUS_FALSE
+											},
+											0,
+											0.0
+										}; // All component specific information is stored in this CIMAR JAUS Library data structure
+static JausComponent gpos = &gposStruct;
+static JausNode gposNode;
+static JausSubsystem gposSubsystem;
+
+static int gposRun = FALSE;
+static double gposThreadHz = 0;									// Stores the calculated update rate for main state thread
+static int gposThreadRunning = FALSE;
+static pthread_t gposThreadId = 0;							// pthread component thread identifier
+
+static Properties gposProperties;
+static NodeManagerInterface gposNmi;	// A data structure containing the Node Manager Interface for this component
+
+//Gpos Variables
+static ReportGlobalPoseMessage gposMessage;
+static JausBoolean gposScActive = JAUS_FALSE;
+
+// Function: 	gposStartup
+// Access:		Public	
+// Description: This function allows the abstracted component functionality contained in this file to be started from an external source.
+// 				It must be called first before the component state machine and node manager interaction will begin
+//				Each call to "gposStartup" should be followed by one call to the "gposShutdown" function
+int gposStartup(void)
+{
+	FILE * propertyFile;
+	pthread_attr_t attr;	// Thread attributed for the component threads spawned in this function
+	
+	if(gpos->state == JAUS_SHUTDOWN_STATE)	// Execute the startup routines only if the component is not running
+	{
+		propertyFile = fopen("./config/gpos.conf", "r");
+		if(propertyFile)
+		{
+			gposProperties = propertiesCreate();
+			gposProperties = propertiesLoad(gposProperties, propertyFile);
+			fclose(propertyFile);
+		}
+		else
+		{
+			cError("gpos: Cannot find or open properties file\n");
+			return GPOS_LOAD_CONFIGURATION_ERROR;
+		}
+		
+		// Check in to the Node Manager and obtain Instance, Node and Subsystem IDs
+		gposNode = jausNodeCreate();
+		gposSubsystem = jausSubsystemCreate();
+		gposNode->subsystem = gposSubsystem;
+		gpos->node = gposNode;
+		gpos->services = jausServicesCreate();
+		gpos->state = JAUS_INITIALIZE_STATE; // Set the state of the JAUS state machine to INITIALIZE
+		
+		gposNmi = nodeManagerOpen(gpos); 
+		if(gposNmi == NULL)
+		{
+			cError("gpos: Could not open connection to node manager\n");
+			return GPOS_NODE_MANAGER_OPEN_ERROR; 
+		}
+
+		pthread_attr_init(&attr);
+		pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+
+		gposRun = TRUE;
+
+		if(pthread_create(&gposThreadId, &attr, gposThread, NULL) != 0)
+		{
+			cError("gpos: Could not create gposThread\n");
+			gposShutdown();
+			pthread_attr_destroy(&attr);
+			return GPOS_THREAD_CREATE_ERROR;
+		}
+		pthread_attr_destroy(&attr);
+	}
+	else
+	{
+		cError("gpos: Attempted startup while not shutdown\n");
+		return GPOS_STARTUP_BEFORE_SHUTDOWN_ERROR;
+	}
+	
+	return 0;
+}
+
+// Function: gposShutdown
+// Access:		Public	
+// Description:	This function allows the abstracted component functionality contained in this file to be stoped from an external source.
+// 				If the component is in the running state, this function will terminate all threads running in this file
+//				This function will also close the Jms connection to the Node Manager and check out the component from the Node Manager
+int gposShutdown(void)
+{
+	double timeOutSec;
+
+	if(gpos->state != JAUS_SHUTDOWN_STATE)	// Execute the shutdown routines only if the component is running
+	{
+		gposRun = FALSE;
+
+		timeOutSec = getTimeSeconds() + GPOS_THREAD_TIMEOUT_SEC;
+		while(gposThreadRunning)
+		{
+			usleep(100000);
+			if(getTimeSeconds() >= timeOutSec)
+			{
+				pthread_cancel(gposThreadId);
+				gposThreadRunning = FALSE;
+				cError("gpos: gposThread Shutdown Improperly\n");
+				break;
+			}
+		}
+
+		nodeManagerClose(gposNmi); // Close Node Manager Connection
+				
+		jausSubsystemDestroy(gpos->node->subsystem);
+		jausNodeDestroy(gpos->node);
+		jausServicesDestroy(gpos->services);
+	
+		gpos->state = JAUS_SHUTDOWN_STATE;
+
+		propertiesDestroy(gposProperties);
+	}
+
+	return 0;
+}
+
+// The series of functions below allow public access to essential component information
+// Access:		Public (All)
+JausState gposGetState(void)
+{
+	return gpos->state;
+}
+
+JausAddress gposGetAddress(void)
+{
+	return gpos->address;
+}
+
+double gposGetUpdateRate(void)
+{
+	return gposThreadHz; 
+}
+
+double gposGetLatitude(void)
+{
+	return gposMessage->latitudeDegrees;
+}
+
+double gposGetLongitude(void)
+{
+	return gposMessage->longitudeDegrees;
+}
+
+double gposGetYaw(void)
+{
+	return gposMessage->yawRadians;
+}
+
+int gposGetScActive(void)
+{
+	return gposScActive;
+}
+
+// Function: gposThread
+// Access:		Private
+// Description:	All core component functionality is contained in this thread.
+//				All of the JAUS component state machine code can be found here.
+void *gposThread(void *threadData)
+{
+	JausMessage rxMessage;
+	double time, prevTime, nextExcecuteTime = 0.0;
+	struct timespec sleepTime;
+
+	gposThreadRunning = TRUE;
+
+	sleepTime.tv_sec = 0;
+	sleepTime.tv_nsec = 1000;
+
+	time = getTimeSeconds();
+	gpos->state = JAUS_INITIALIZE_STATE; // Set JAUS state to INITIALIZE
+	
+	gposStartupState();
+		
+	while(gposRun) // Execute state machine code while not in the SHUTDOWN state
+	{
+		do
+		{
+			if(nodeManagerReceive(gposNmi, &rxMessage))
+			{
+				cDebug(4, "GPOS: Got message: %s from %d.%d.%d.%d\n", jausMessageCommandCodeString(rxMessage), rxMessage->source->subsystem, rxMessage->source->node, rxMessage->source->component, rxMessage->source->instance);
+				gposProcessMessage(rxMessage);
+			}
+			else 
+			{
+				if(getTimeSeconds() > nextExcecuteTime)
+				{
+					break;
+				}
+				else
+				{
+					nanosleep(&sleepTime, NULL);
+				}
+			}
+		}while(getTimeSeconds() < nextExcecuteTime);
+		
+		prevTime = time;
+		time = getTimeSeconds();
+		gposThreadHz = 1.0/(time-prevTime); // Compute the update rate of this thread
+		
+		switch(gpos->state) // Switch component behavior based on which state the machine is in
+		{
+			case JAUS_INITIALIZE_STATE:
+				gposInitState();
+				break;
+				
+			case JAUS_STANDBY_STATE:
+				gposStandbyState();
+				break;
+				
+			case JAUS_READY_STATE:
+				gposReadyState();
+				break;
+				
+			case JAUS_EMERGENCY_STATE:
+				gposEmergencyState();
+				break;
+				
+			case JAUS_FAILURE_STATE:
+				gposFailureState();
+				break;		
+						
+			case JAUS_SHUTDOWN_STATE:
+				gposRun = FALSE;			
+				break;		
+
+			default:
+				gpos->state = JAUS_FAILURE_STATE; // The default case JAUS_is undefined, therefore go into Failure State
+				break;
+		}	
+		
+		gposAllState();
+		nodeManagerSendCoreServiceConnections(gposNmi);
+
+		nextExcecuteTime = 2.0 * time + 1.0/GPOS_THREAD_DESIRED_RATE_HZ - getTimeSeconds();
+	}	
+	
+	gposShutdownState();
+	
+	usleep(50000);	// Sleep for 50 milliseconds and then exit
+
+	gposThreadRunning = FALSE;
+	
+	return NULL;
+}
+
+// Function: gposProcessMessage
+// Access:		Private
+// Description:	This function is responsible for handling incoming JAUS messages from the Node Manager.
+//				Incoming messages are processed according to message type.
+void gposProcessMessage(JausMessage message)
+{
+	QueryGlobalPoseMessage queryGlobalPose;
+	JausMessage txMessage;
+
+	// This block of code is intended to reject commands from non-controlling components
+	if(gpos->controller.active && message->source->id != gpos->controller.address->id && jausMessageIsRejectableCommand(message) )
+	{
+		cError("gpos: Received command message %s from non-controlling component.\n", jausMessageCommandCodeString(message));
+		jausMessageDestroy(message); // Ignore this message
+		return;		
+	}	
+
+	switch(message->commandCode) // Switch the processing algorithm according to the JAUS message type
+	{
+		case JAUS_QUERY_GLOBAL_POSE:
+			queryGlobalPose = queryGlobalPoseMessageFromJausMessage(message);
+			if(queryGlobalPose)
+			{
+				gposMessage->destination->id = queryGlobalPose->source->id;
+				gposMessage->presenceVector = queryGlobalPose->presenceVector;
+				gposMessage->sequenceNumber = 0;
+				gposMessage->scFlag = 0;
+				
+				txMessage = reportGlobalPoseMessageToJausMessage(gposMessage);
+				nodeManagerSend(gposNmi, txMessage);		
+				jausMessageDestroy(txMessage);
+				
+				queryGlobalPoseMessageDestroy(queryGlobalPose);
+			}
+			else
+			{
+				cError("gpos: Error unpacking %s message.\n", jausMessageCommandCodeString(message));
+			}
+			jausMessageDestroy(message);
+			break;
+		
+		default:
+			defaultJausMessageProcessor(message, gposNmi, gpos);
+			break;
+	}
+}
+
+void gposStartupState(void)
+{
+	JausService service;
+	
+	// Populate Core Service
+	if(!jausServiceAddCoreServices(gpos->services))
+	{
+		cError("gpos: Addition of Core Services FAILED! Switching to FAILURE_STATE\n");
+		gpos->state = JAUS_FAILURE_STATE;
+	}
+	// USER: Add the rest of your component specific service(s) here
+	service = jausServiceCreate(gpos->address->component);
+	
+
+	gposMessage = reportGlobalPoseMessageCreate();
+	gposMessage->source->id = gpos->address->id;
+
+	//add support for ReportGlovalPose Service Connections
+	scManagerAddSupportedMessage(gposNmi, JAUS_REPORT_GLOBAL_POSE);
+}
+
+void gposInitState(void)
+{
+	gpos->state = JAUS_READY_STATE;
+}
+
+void gposStandbyState(void)
+{
+	// USER: Insert Standby Code Here
+}
+
+void gposReadyState(void)
+{
+	JausMessage txMessage;
+	ServiceConnection scList;
+	ServiceConnection sc;
+	char buf[64] = {0};
+	char buf2[64] = {0};
+	
+	PointLla vehiclePosLla;
+	
+	vehiclePosLla = vehicleSimGetPositionLla();
+	if(vehiclePosLla)
+	{		
+		gposMessage->latitudeDegrees = vehiclePosLla->latitudeRadians * DEG_PER_RAD;
+		gposMessage->longitudeDegrees = vehiclePosLla->longitudeRadians * DEG_PER_RAD;
+	}
+	
+	gposMessage->yawRadians = -(vehicleSimGetH() - PI/2.0);
+
+	gposMessage->elevationMeters = 0;
+	gposMessage->positionRmsMeters = 1.0;
+	gposMessage->rollRadians = 0.0;
+	gposMessage->pitchRadians = 0.0;
+	gposMessage->attitudeRmsRadians = 0.05;
+	gposMessage->timeStamp = 0;
+
+	// Send message
+	if(scManagerQueryActiveMessage(gposNmi, JAUS_REPORT_GLOBAL_POSE))
+	{
+		gposScActive = JAUS_TRUE;
+			
+		scList = scManagerGetSendList(gposNmi, JAUS_REPORT_GLOBAL_POSE);
+		sc = scList;
+		while(sc)
+		{
+			gposMessage->destination->id = sc->address->id;
+			gposMessage->presenceVector = sc->presenceVector;
+			gposMessage->sequenceNumber = sc->sequenceNumber;
+			gposMessage->scFlag = JAUS_SERVICE_CONNECTION_MESSAGE;
+			
+			txMessage = reportGlobalPoseMessageToJausMessage(gposMessage);
+			nodeManagerSend(gposNmi, txMessage);
+			jausMessageDestroy(txMessage);
+	
+			jausAddressToString(gposMessage->source, buf);
+			jausAddressToString(gposMessage->destination, buf2);
+			cDebug(9, "Sent GPOS SC from %s to %s\n", buf, buf2);
+	
+			sc = sc->nextSc;
+		}
+		
+		scManagerDestroySendList(scList);
+	}
+	else
+	{
+		gposScActive = JAUS_FALSE;
+	}
+}
+
+void gposEmergencyState(void)
+{
+	// USER: Insert Emergency Code Here
+}
+
+void gposFailureState(void)
+{
+	// USER: Insert Failure Code Here
+}
+
+void gposShutdownState(void)
+{
+	RejectComponentControlMessage rejectComponentControl;
+	JausMessage txMessage;
+		
+	if(gpos->controller.active)
+	{
+		// Terminate control of current component
+		rejectComponentControl = rejectComponentControlMessageCreate();
+		rejectComponentControl->source->id = gpos->address->id;
+		rejectComponentControl->destination->id = gpos->controller.address->id;
+
+		txMessage = rejectComponentControlMessageToJausMessage(rejectComponentControl);
+		nodeManagerSend(gposNmi, txMessage);
+		jausMessageDestroy(txMessage);
+
+		rejectComponentControlMessageDestroy(rejectComponentControl);
+	}
+	
+	// Remove support for ReportGlovalPose Service Connections
+	scManagerRemoveSupportedMessage(gposNmi, JAUS_REPORT_GLOBAL_POSE);
+
+	reportGlobalPoseMessageDestroy(gposMessage);
+}
+
+void gposAllState(void)
+{
+	// USER: Insert Code Here that is Common to All states
+}
---
+++ openjaus/trunk/Components/examples/vehicle/src/main.c
@@ -0,0 +1,452 @@
+#include <cimar.h>
+#include <cimar/jaus.h>
+#include <ncurses.h>
+#include <ctype.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "simulator.h"
+#include "pd.h"
+#include "gpos.h"
+#include "vss.h"
+#include "vehicleSim.h"
+#include "wd.h"
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#define DEFAULT_STRING_LENGTH 128
+
+#define KEYBOARD_LOCK_TIMEOUT_SEC	60.0
+
+static int mainRunning = FALSE;
+
+// Refresh screen in curses mode
+void updateScreen(int keyboardLock, int keyPress)
+{
+	int row = 0;
+	int col = 0;
+	char string[256] = {0};
+	PointLla vehiclePosLla;
+	static int lastChoice = '1';
+	
+	if(!keyboardLock && keyPress != -1 && keyPress != 27 && keyPress != 12) //Magic Numbers: 27 = ESC, 12 = Ctrl+l
+	{
+		switch(keyPress)
+		{
+			case ' ':
+				wdToggleRequestControl();
+				break;
+			
+			case 'S':
+				wdSetSpeed();
+				break;
+			
+			case 'W':
+				wdCreateWaypoint();
+				break;
+			
+			default:
+				lastChoice = keyPress;			
+		}
+	}
+
+	clear();
+
+	mvprintw(row,35,"Keyboard Lock:	%s", keyboardLock?"ON, Press ctrl+L to unlock":"OFF, Press ctrl+L to lock");
+	
+	mvprintw(row++,0,"+---------------------------+");
+	mvprintw(row++,0,"|     Component Menu        |");
+	mvprintw(row++,0,"|                           |");
+	mvprintw(row++,0,"| 1. Vehicle Sim            |");
+	mvprintw(row++,0,"| 2. Primitive Driver       |");
+	mvprintw(row++,0,"| 3. GPOS / VSS             |");
+	mvprintw(row++,0,"| 4. Waypoint Driver        |");
+	mvprintw(row++,0,"|                           |");		
+	mvprintw(row++,0,"| ESC to Exit               |");		
+	mvprintw(row++,0,"+---------------------------+");
+
+	row = 2;
+	col = 40;
+	switch(lastChoice)
+	{
+		case '1':
+			mvprintw(row++,col,"Vehicle Simulator");	
+			mvprintw(row++,col,"VS Update Rate:	%7.2f", vehicleSimGetUpdateRate());	
+			mvprintw(row++,col,"VS Run/Pause:	%s", vehicleSimGetRunPause() == VEHICLE_SIM_PAUSE ? "Pause" : "Run");	
+			row++;
+			mvprintw(row++,col,"VS Vehicle X:\t%9.2f", vehicleSimGetX());	
+			mvprintw(row++,col,"VS Vehicle Y:\t%9.2f", vehicleSimGetY());	
+			mvprintw(row++,col,"VS Vehicle H:\t%9.2f", vehicleSimGetH());	
+			mvprintw(row++,col,"VS Vehicle Speed: %7.2f", vehicleSimGetSpeed());	
+		
+			row++;
+			mvprintw(row++,col,"VS Throttle:\t%9.2f", vehicleSimGetLinearEffortX());	
+			mvprintw(row++,col,"VS Brake:\t%9.2f", vehicleSimGetResistiveEffortX());	
+			mvprintw(row++,col,"VS Steering:\t%9.2f", vehicleSimGetRotationalEffort());	
+		
+			row++;	
+			vehiclePosLla = vehicleSimGetPositionLla();
+			mvprintw(row++,col,"VS Vehicle Latitude:  %+10.7f", vehiclePosLla? vehiclePosLla->latitudeRadians*DEG_PER_RAD : 0.0);
+			mvprintw(row++,col,"VS Vehicle Longitude: %+10.7f", vehiclePosLla? vehiclePosLla->longitudeRadians*DEG_PER_RAD : 0.0);
+			break;
+	
+		case '2':		
+			mvprintw(row++,col,"Primitive Driver");	
+			mvprintw(row++,col,"PD Update Rate:	%5.2f", pdGetUpdateRate());	
+			jausAddressToString(pdGetAddress(), string );	
+			mvprintw(row++,col,"PD Address:\t%s", string);	
+			mvprintw(row++,col,"PD State:\t%s", jausStateGetString(pdGetState()));	
+			
+			row++;
+			if(pdGetControllerStatus())
+			{
+				jausAddressToString(pdGetControllerAddress(), string);	
+				mvprintw(row++,col,"PD Controller:	%s", string);	
+			}
+			else
+			{
+				mvprintw(row++,col,"PD Controller:	None");	
+			}
+			mvprintw(row++,col,"PD Controller SC:	%s", pdGetControllerScStatus()?"Active":"Inactive");	
+			mvprintw(row++,col,"PD Controller State:	%s", jausStateGetString(pdGetControllerState()));	
+			
+			row++;
+			mvprintw(row++,col,"PD Prop Effort X: %0.0lf", pdGetWrenchEffort()? pdGetWrenchEffort()->propulsiveLinearEffortXPercent:-1.0);	
+			mvprintw(row++,col,"PD Rstv Effort X: %0.0lf", pdGetWrenchEffort()? pdGetWrenchEffort()->resistiveLinearEffortXPercent:-1.0);	
+			mvprintw(row++,col,"PD Rtat Effort Z: %0.0lf", pdGetWrenchEffort()? pdGetWrenchEffort()->propulsiveRotationalEffortZPercent:-1.0);	
+			break;
+		
+		case '3':
+			mvprintw(row++,col,"Global Pose Sensor");	
+			mvprintw(row++,col,"GPOS Update Rate: %7.2f", gposGetUpdateRate());	
+			jausAddressToString(gposGetAddress(), string );	
+			mvprintw(row++,col,"GPOS Address:\t    %s", string);	
+			mvprintw(row++,col,"GPOS State:\t    %s", jausStateGetString(gposGetState()));	
+			mvprintw(row++,col,"GPOS SC State:\t    %s", gposGetScActive()? "Active" : "Inactive");	
+			
+			row++;
+			mvprintw(row++,col,"Velocity State Sensor");	
+			mvprintw(row++,col,"VSS Update Rate:  %7.2f", vssGetUpdateRate());	
+			jausAddressToString(vssGetAddress(), string );	
+			mvprintw(row++,col,"VSS Address:\t    %s", string);	
+			mvprintw(row++,col,"VSS State:\t    %s", jausStateGetString(vssGetState()));	
+			mvprintw(row++,col,"VSS SC State:\t    %s", vssGetScActive()? "Active" : "Inactive");	
+			break;
+		
+		case '4':
+			mvprintw(row++,col,"Waypoint Driver");	
+			mvprintw(row++,col,"WD Update Rate: %7.2f", wdGetUpdateRate());	
+
+			jausAddressToString(wdGetAddress(), string);	
+			mvprintw(row++,col,"WD Address:\t  %s", string);	
+			mvprintw(row++,col,"WD State:\t  %s", jausStateGetString(wdGetState()));
+			
+			if(wdGetControllerAddress())
+			{
+				jausAddressToString(wdGetControllerAddress(), string);	
+				mvprintw(row++,col,"WD Controller:\t  %s", string);	
+			}
+			else
+			{
+				mvprintw(row++,col,"WD Controller:\t  None");	
+			}
+
+			row = 11;
+			col = 2;
+			mvprintw(row++,col,"GPOS SC:\t    %s", wdGetGposScStatus()? "Active" : "Inactive");
+			mvprintw(row++,col,"VSS SC:\t    %s", wdGetVssScStatus()? "Active" : "Inactive");
+			mvprintw(row++,col,"PD Wrench SC:\t    %s", wdGetPdWrenchScStatus()? "Active" : "Inactive");
+			mvprintw(row++,col,"PD State SC:\t    %s", wdGetPdStatusScStatus()? "Active" : "Inactive");
+			row++;
+			mvprintw(row++,col,"WD Request Control:\t    %s", wdGetRequestControl()? "True" : "False");
+			mvprintw(row++,col,"WD Control:\t    %s", wdGetInControlStatus()? "True" : "False");
+			mvprintw(row++,col,"PD State:\t    %s", jausStateGetString(wdGetPdState()));
+			
+			row = 11;
+			col = 40;
+			if(wdGetGlobalWaypoint())
+			{
+				mvprintw(row++,col,"Global Waypoint: (%9.7lf,%9.7lf)", wdGetGlobalWaypoint()->latitudeDegrees, wdGetGlobalWaypoint()->longitudeDegrees);
+			}
+			else
+			{
+				mvprintw(row++,col,"Global Waypoint: None");
+			}
+					
+			if(wdGetTravelSpeed())
+			{
+				mvprintw(row++,col,"Travel Speed: %7.2f", wdGetTravelSpeed()->speedMps);
+			}
+			else
+			{
+				mvprintw(row++,col,"Travel Speed: None");				
+			}
+
+			mvprintw(row++,col,"dSpeedMps: %7.2f", wdGetDesiredVehicleState()? wdGetDesiredVehicleState()->desiredSpeedMps : 0.0);
+			mvprintw(row++,col,"dPhi:      %7.2f", wdGetDesiredVehicleState()? wdGetDesiredVehicleState()->desiredPhiEffort : 0.0);
+			
+			break;
+
+		default:
+			mvprintw(row++,col,"NONE.");	
+			break;
+	}
+	
+	move(24,0);
+	refresh();
+}
+
+int main(int argCount, char **argString)
+{
+	int i = 0;
+	int choice=0;
+	int verbose = FALSE;
+	int keyboardLock = FALSE;
+	int debugLevel = 0;
+	double keyboardLockTime = getTimeSeconds() + KEYBOARD_LOCK_TIMEOUT_SEC;
+	struct termios newTermio;
+	struct termios storedTermio;
+	FILE *logFile = NULL;
+	char logFileStr[DEFAULT_STRING_LENGTH] = "";
+	char timeString[DEFAULT_STRING_LENGTH] = "";
+	char debugLogicString[DEFAULT_STRING_LENGTH] = "";
+	time_t timeStamp;
+	
+	//Get and Format Time String
+	time(&timeStamp);
+	strftime(timeString, DEFAULT_STRING_LENGTH-1, "%m-%d-%Y %X", localtime(&timeStamp));
+
+	system("clear");
+
+	printf("main: CIMAR Core Executable: %s\n", timeString);
+
+	// Make directory and set permisions on CIMAR Directory to fully read, write, execute
+	if(mkdir("/var/log/CIMAR/", 0777) < 0) 
+	{
+		if(errno != EEXIST)
+		{
+			perror("main: Error");
+			return -1;
+		}
+	}
+
+	for(i=1; i<argCount; i++)
+	{
+		if(argString[i][0] == '-')
+		{
+			switch(argString[i][1])
+			{
+				case 'v':
+					verbose = TRUE;
+					setLogVerbose(TRUE);
+					break;
+					
+				case 'd':
+					if(argString[i][2] == '+') 
+					{
+						setDebugLogic(DEBUG_GREATER_THAN);
+						sprintf(debugLogicString, "Greater than or equal to: ");
+						if(argString[i][3] >= '0' && argString[i][3] <= '9')
+						{
+							debugLevel = atoi(&argString[i][3]);
+						}
+						else
+						{
+							printf("main: Incorrect use of arguments\n");
+							break;
+						}
+					}
+					else if(argString[i][2] == '-')
+					{
+						setDebugLogic(DEBUG_LESS_THAN);
+						sprintf(debugLogicString, "Less than or equal to: ");
+						if(argString[i][3] >= '0' && argString[i][3] <= '9')
+						{
+							debugLevel = atoi(&argString[i][3]);
+						}
+						else
+						{
+							printf("main: Incorrect use of arguments\n");
+							break;
+						}
+					}
+					else if(argString[i][2] == '=')
+					{
+						setDebugLogic(DEBUG_EQUAL_TO);
+						if(argString[i][3] >= '0' && argString[i][3] <= '9')
+						{
+							debugLevel = atoi(&argString[i][3]);
+						}
+						else
+						{
+							printf("main: Incorrect use of arguments\n");
+							break;
+						}
+					}
+					else if(argString[i][2] >= '0' && argString[i][2] <= '9')
+					{
+						debugLevel = atoi(&argString[i][2]);
+					}
+					else
+					{
+						printf("main: Incorrect use of arguments\n");
+						break;
+					}
+					printf("main: Switching to debug level: %s%d\n", debugLogicString, debugLevel);
+					setDebugLevel(debugLevel);
+					break;
+					
+				case 'l':
+					if(argCount > i+1 && argString[i+1][0] != '-')
+					{
+						logFile = fopen(argString[i+1], "w");
+						if(logFile != NULL)
+						{
+							fprintf(logFile, "CIMAR %s Log -- %s\n", argString[0], timeString);
+							setLogFile(logFile);
+						}
+						else printf("main: Error creating log file, switching to default\n");
+					}
+					else
+					{
+						printf("main: Incorrect use of arguments\n");
+					}
+					break;
+
+				default:
+					printf("main: Incorrect use of arguments\n");
+					break;
+			}
+		}
+	}
+
+	if(logFile == NULL)
+	{
+		i = strlen(argString[0]) - 1;
+		while(i>0 && argString[0][i-1] != '/') i--;
+				
+		sprintf(logFileStr, "/var/log/CIMAR/%s.log", &argString[0][i]);
+		printf("main: Creating log: %s\n", logFileStr);
+		logFile = fopen(logFileStr, "w");
+		if(logFile != NULL)
+		{
+			fprintf(logFile, "CIMAR %s Log -- %s\n", argString[0], timeString);
+			setLogFile(logFile);
+		}
+		else printf("main: ERROR: Could not create log file\n");
+		// BUG: Else pError here
+	}
+		
+	cDebug(1, "main: Starting Up %s Node Software\n", simulatorGetName());
+	if(simulatorStartup())
+	{
+		cError("main: %s Node Startup failed\n", simulatorGetName());
+		cDebug(1, "main: Exiting %s Node Software\n", simulatorGetName());
+		return 0;
+	}
+
+	if(verbose)
+	{
+		tcgetattr(0,&storedTermio);
+		memcpy(&newTermio,&storedTermio,sizeof(struct termios));
+		
+		// Disable canonical mode, and set buffer size to 0 byte(s)
+		newTermio.c_lflag &= (~ICANON);
+		newTermio.c_lflag &= (~ECHO);
+		newTermio.c_cc[VTIME] = 0;
+		newTermio.c_cc[VMIN] = 0;
+		tcsetattr(0,TCSANOW,&newTermio);
+	}
+	else
+	{	
+		// Start up Curses window
+		initscr();
+		cbreak();
+		noecho();
+		nodelay(stdscr, 1);	// Don't wait at the getch() function if the user hasn't hit a key
+		keypad(stdscr, 1); // Allow Function key input and arrow key input
+	}
+	
+	mainRunning = TRUE;
+	
+	while(mainRunning)
+	{
+		if(verbose)
+		{
+			choice = getc(stdin);
+		}
+		else // Not in verbose mode
+		{
+			choice = getch(); // Get the key that the user has selected
+			updateScreen(keyboardLock, choice);		
+		}
+
+		if(choice > -1)
+		{
+			keyboardLockTime = getTimeSeconds() + KEYBOARD_LOCK_TIMEOUT_SEC;
+			if(choice == 12) // 12 == 'ctrl + L'
+			{
+				keyboardLock = !keyboardLock;
+			}
+		}
+		else
+		{
+			if(getTimeSeconds() > keyboardLockTime)
+			{
+				keyboardLock = TRUE;
+			}
+		}
+		
+		
+		if(!keyboardLock)
+		{
+			switch(choice)
+			{
+				// USER: Add key press event options here
+
+				case 27: // Escape Key Pressed
+					mainRunning = FALSE;
+					break;
+					
+				default:
+					break;
+			}
+		}
+				
+		usleep(50000);
+	}
+	
+	if(verbose)
+	{
+		tcsetattr(0,TCSANOW,&storedTermio);
+	}
+	else
+	{
+		// Stop Curses
+		clear();
+		endwin();
+	}
+	
+	cDebug(1, "main: Shutting Down %s Node Software\n", simulatorGetName());
+	simulatorShutdown();
+	
+	if(logFile != NULL)
+	{
+		fclose(logFile);
+	}
+	
+	return 0;
+}
