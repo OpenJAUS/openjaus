fc9358d38d4f20afb7f12fb510a27591b0eff246
kentd <kentd@localhost>
2006-12-21 15:42:20 +0000

    Fixes #35

--- openjaus/trunk/Core/libnodeManager/src/defaultJausMessageProcessor.c
+++ openjaus/trunk/Core/libnodeManager/src/defaultJausMessageProcessor.c
@@ -554,7 +554,7 @@ void defaultJausMessageProcessor(JausMessage message, NodeManagerInterface nmi, 
 
 				reportServices->destination->id = message->source->id;
 				reportServices->source->id = nmi->cmpt->address->id;
-				jausServicesDestroy(resportServices->jausServices);
+				jausServicesDestroy(reportServices->jausServices);
 				reportServices->jausServices = jausServicesDuplicate(nmi->cmpt->services);
 
 				txMessage = reportServicesMessageToJausMessage(reportServices);
