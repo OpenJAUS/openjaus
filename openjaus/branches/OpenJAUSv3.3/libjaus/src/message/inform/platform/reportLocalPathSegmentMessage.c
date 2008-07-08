5f63ecdba9596041ba75c345dfee05ab34f5d9ae
kentd <kentd@localhost>
2008-07-08 22:33:35 +0000

    Updated header files for OpenJAUS 3.3.0 release.

--- openjaus/branches/OpenJAUSv3.3/libjaus/src/message/inform/platform/reportLocalPathSegmentMessage.c
+++ openjaus/branches/OpenJAUSv3.3/libjaus/src/message/inform/platform/reportLocalPathSegmentMessage.c
@@ -1,5 +1,5 @@
 /*****************************************************************************
- *  Copyright (c) 2008, OpenJAUS.com
+ *  Copyright (c) 2008, University of Florida
  *  All rights reserved.
  *  
  *  This file is part of OpenJAUS.  OpenJAUS is distributed under the BSD 
@@ -35,9 +35,9 @@
 //
 // Written By: Danny Kent (jaus AT dannykent DOT com), Tom Galluzzo (galluzzo AT gmail DOT com)
 //
-// Version: 3.3 BETA
+// Version: 3.3.0
 //
-// Date: 04/15/08
+// Date: 07/09/08
 //
 // Description: This file defines the functionality of a ReportLocalPathSegmentMessage
 
