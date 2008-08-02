c642c10d5c6c573a00b6241540d792096a8c3b13
galluzzt <galluzzt@localhost>
2008-08-02 14:36:54 +0000

    Commented out changes for temporary compile fix

--- openjaus/trunk/OpenJAUSv3.3.1/libopenJaus/src/nodeManager/transport/Judp1Message.cpp
+++ openjaus/trunk/OpenJAUSv3.3.1/libopenJaus/src/nodeManager/transport/Judp1Message.cpp
@@ -3,24 +3,90 @@
 
 Judp1Message::Judp1Message()
 {
+	hcHeaderNumber = 0;
+	hcLength = 0;
+	hcFlags = 0;
+	messageLength = 0;
+	message = NULL;
+	
+	index = 0;
+	buffer = NULL;
+	bufferSizeBytes = 0;
 }
-
+	
 Judp1Message::~Judp1Message(void)
 {
-
+	
 }
 
-//int Judp1Message::getMessageType(void)
-//{
-//	return JUDP_JAUS_MESSAGE_TYPE;	// Only JAUS message type supported by JUDP 1.0 
-//}
-//
-//char* Judp1Message::getPayloadBuffer(void)
-//{
-//	return NULL;
-//}
-//
-//JausMessage Judp1Message::toJausMessage(void)
-//{
-//	return NULL;
-//}
+int Judp1Message::getHcHeaderNumber(void)
+{
+	return hcHeaderNumber;
+}
+
+bool Judp1Message::setHcHeaderNumber(int hcHeaderNumber)
+{
+	return true;
+}
+
+int Judp1Message::getHcLength(void)
+{
+	return hcLength;
+}
+
+bool Judp1Message::setHcLength(int hcLength)
+{
+	return true;	
+}
+
+
+int Judp1Message::getHcFlags(void)
+{
+	return hcFlags;	
+}
+
+bool Judp1Message::setHcFlags(int hcFlags)
+{
+	
+	return true;
+}
+
+
+int Judp1Message::getMessageLength(void)
+{
+	return messageLength;	
+}
+
+bool Judp1Message::setMessageLength(int messageLength)
+{
+	
+	return true;
+}
+
+
+JausMessage Judp1Message::getJausMessage(void)
+{
+	return NULL;
+}
+
+bool Judp1Message::setJausMessage(JausMessage message)
+{
+	
+	return true;
+}
+
+
+int Judp1Message::toBuffer(unsigned char *buffer, int bufferSizeBytes)
+{
+	return 0;
+}
+
+int Judp1Message::fromBuffer(unsigned char *buffer, int bufferSizeBytes)
+{
+	return 0;
+}
+
+int Judp1Message::getSizeBytes(void)
+{
+	return 0;
+}
--- openjaus/trunk/OpenJAUSv3.3.1/libopenJaus/src/nodeManager/transport/JudpMessage.cpp
+++ openjaus/trunk/OpenJAUSv3.3.1/libopenJaus/src/nodeManager/transport/JudpMessage.cpp
@@ -1,31 +1,126 @@
+#include <jaus.h>
+#include "nodeManager/transport/Transportable.h"
+#include "nodeManager/transport/Judp1Message.h"
+#include "nodeManager/transport/Judp2Message.h"
 #include "nodeManager/transport/JudpMessage.h"
+
+JudpMessage::JudpMessage()
+{
+	setVersion(JUDP_VERSION_UNDEFINED);
+	judp1Message = new Judp1Message();
+	//judp2Message = new Judp2Message();
+	messages.empty();
+}
+
+JudpMessage::JudpMessage(int version)
+{
+	JudpMessage();
+	setVersion(version);
+}
 
 JudpMessage::~JudpMessage(void)
 {
-
 }
 
-//JausAddress JudpMessage::getSourceAddress(void)
-//{
-//	return NULL;
-//}
+int JudpMessage::getVersion(void)
+{
+	return version;
+}
+
+bool JudpMessage::setVersion(int version)
+{
+	this->version = version;
+	return false;
+}
+
+Transportable *JudpMessage::popMessage(void)
+{
+	int messageIndex = 0;
+	return messages[messageIndex];
+}
+
+bool JudpMessage::pushMessage(Transportable *message)
+{
+	this->messages.push_back(message);
+	return true;
+}
+
+int JudpMessage::toBuffer(unsigned char *buffer, int bufferSizeBytes)
+{
+	return 0;
+}
+
+int JudpMessage::fromBuffer(unsigned char *buffer, int bufferSizeBytes)
+{
+	Transportable *message = NULL;
+	int index = 0;
+	int bytesUnpacked = 0;
+
+	index = 1;
+	
+	while(index < bufferSizeBytes)
+	{
+		switch(version)
+		{
+			case JUDP_VERSION_1_0:
+				message = new Judp1Message();
+				bytesUnpacked = judp1Message->fromBuffer(buffer + index, bufferSizeBytes - index);
+				break;
+
+			case JUDP_VERSION_2_0:
+				//message = new Judp2Message();
+				bytesUnpacked = 0;//judp2Message->fromBuffer(buffer + index, bufferSizeBytes - index);
+				break;
+				
+			default:
+				// TODO: Throw error
+				break;
+		}		
+
+		this->messages.push_back(message);
+		index += bytesUnpacked;
+		
+		if(!bytesUnpacked)
+		{
+			break;
+		}
+	}
+	
+	return index;
+}
+	
+int JudpMessage::getSizeBytes(void)
+{
+	//Transportable *message = NULL;
+	int sizeBytes = 1;
+//	int bytesUnpacked = 0;
+//	
+//	while(sizeBytes < bufferSizeBytes)
+//	{
+//		switch(version)
+//		{
+//			case JUDP_VERSION_1_0:
+//				bytesUnpacked = judp1Message->getSizeBytes();
+//				break;
 //
-//JausAddress JudpMessage::getDestinationAddress(void)
-//{
-//	return NULL;
-//}
+//			case JUDP_VERSION_2_0:
+//				bytesUnpacked = judp2Message->getSizeBytes();
+//				break;
+//				
+//			default:
+//				// TODO: Throw error
+//				break;
+//		}		
 //
-//int JudpMessage::getMessageType(void)
-//{
-//	return 0; 
-//}
+//		if(!bytesUnpacked)
+//		{
+//			break;
+//		}
 //
-//char* JudpMessage::getPayloadBuffer(void)
-//{
-//	return NULL;
-//}
+//		sizeBytes += bytesUnpacked;
+//	}
 //
-//JausMessage JudpMessage::toJausMessage(void)
-//{
-//	return NULL;
-//}
+	return sizeBytes;
+}
+
+
