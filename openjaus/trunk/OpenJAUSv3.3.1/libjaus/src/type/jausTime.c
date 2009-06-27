04f9e6413b9d1b4cf7e9d8e4b98feb3d260ce22d
kentd <kentd@localhost>
2009-06-27 04:15:27 +0000

    Some minor edits. Added functionality to the jausTime type.

--- openjaus/trunk/OpenJAUSv3.3.1/libjaus/src/type/jausTime.c
+++ openjaus/trunk/OpenJAUSv3.3.1/libjaus/src/type/jausTime.c
@@ -1,12 +1,12 @@
 /*****************************************************************************
  *  Copyright (c) 2008, University of Florida
  *  All rights reserved.
- *  
- *  This file is part of OpenJAUS.  OpenJAUS is distributed under the BSD 
+ *
+ *  This file is part of OpenJAUS.  OpenJAUS is distributed under the BSD
  *  license.  See the LICENSE file for details.
- * 
- *  Redistribution and use in source and binary forms, with or without 
- *  modification, are permitted provided that the following conditions 
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
  *  are met:
  *
  *     * Redistributions of source code must retain the above copyright
@@ -15,20 +15,20 @@
  *       copyright notice, this list of conditions and the following
  *       disclaimer in the documentation and/or other materials provided
  *       with the distribution.
- *     * Neither the name of the University of Florida nor the names of its 
- *       contributors may be used to endorse or promote products derived from 
+ *     * Neither the name of the University of Florida nor the names of its
+ *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
  *
- *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
- *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
- *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
  *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
- *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ****************************************************************************/
 // File Name: jausTime.c
@@ -53,7 +53,7 @@ JausBoolean calculateDateStamp(JausTime time);
 JausTime jausTimeCreate(void)
 {
 	JausTime time = (JausTime)malloc(sizeof(JausTimeStruct));
-	
+
 	time->timeStamp = newJausUnsignedInteger(0);
 	time->dateStamp = newJausUnsignedShort(0);
 
@@ -76,12 +76,12 @@ void jausTimeDestroy(JausTime time)
 JausTime jausTimeClone(JausTime time)
 {
 	JausTime newTime = (JausTime)malloc(sizeof(JausTimeStruct));
-	
+
 	if (newTime)
 	{
 		newTime->timeStamp = time->timeStamp;
 		newTime->dateStamp = time->dateStamp;
-	
+
 		newTime->millisec = time->millisec;
 		newTime->second = time->second;
 		newTime->minute = time->minute;
@@ -90,7 +90,7 @@ JausTime jausTimeClone(JausTime time)
 		newTime->month = time->month;
 		newTime->year = time->year;
 	}
-	
+
 	return newTime;
 }
 
@@ -101,7 +101,7 @@ JausBoolean jausTimeCopy(JausTime dstTime, JausTime srcTime)
 	{
 		return JAUS_FALSE;
 	}
-	
+
 	dstTime->timeStamp = srcTime->timeStamp;
 	dstTime->dateStamp = srcTime->dateStamp;
 
@@ -112,19 +112,39 @@ JausBoolean jausTimeCopy(JausTime dstTime, JausTime srcTime)
 	dstTime->day = srcTime->day;
 	dstTime->month = srcTime->month;
 	dstTime->year = srcTime->year;
-	
+
 	return JAUS_TRUE;
 }
-
 
 JausBoolean jausTimeSetCurrentTime(JausTime jausTime)
 {
 	struct tm *gmTime;
 	time_t timeVal;
-	
+
 	// Get SystemTime
 	time(&timeVal);
-	
+
+	gmTime = gmtime(&timeVal);
+
+	// Populate members
+	jausTime->millisec =	(unsigned short) 0;
+	jausTime->second = 		(unsigned short) gmTime->tm_sec;
+	jausTime->minute = 		(unsigned short) gmTime->tm_min;
+	jausTime->hour = 		(unsigned short) gmTime->tm_hour;
+	jausTime->day = 		(unsigned short) gmTime->tm_mday;
+	jausTime->month = 		(unsigned short) gmTime->tm_mon;
+	jausTime->year = 		(unsigned short) gmTime->tm_year + 1900;
+
+	// Calculate TimeStamp & DataStamp
+	calculateTimeStamp(jausTime);
+	calculateDateStamp(jausTime);
+
+	return JAUS_TRUE;
+}
+
+JausBoolean jausTimeSetTime(JausTime jausTime, time_t timeVal)
+{
+	struct tm *gmTime;
 	gmTime = gmtime(&timeVal);
 
 	// Populate members
@@ -148,7 +168,7 @@ JausBoolean jausTimeToString(JausTime time, char *buffer, size_t buffSize)
 	const char *months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
 	if(time)
-	{		
+	{
 		sprintf(buffer, "%s %02d, %04d  %02d:%02d:%02d.%d\n", months[time->month], time->day, time->year, time->hour, time->minute, time->second, time->millisec);
 		return JAUS_TRUE;
 	}
@@ -161,7 +181,7 @@ JausBoolean jausTimeDateToString(JausTime time, char *buffer)
   const char *months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
   if(time)
-  {   
+  {
     sprintf(buffer, "%s %02d, %04d", months[time->month], time->day, time->year);
     return JAUS_TRUE;
   }
@@ -172,7 +192,7 @@ JausBoolean jausTimeDateToString(JausTime time, char *buffer)
 JausBoolean jausTimeTimeToString(JausTime time, char *buffer)
 {
   if(time)
-  {   
+  {
     sprintf(buffer, "%02d:%02d:%02d.%d", time->hour, time->minute, time->second, time->millisec);
     return JAUS_TRUE;
   }
@@ -184,14 +204,14 @@ JausBoolean jausTimeStampFromBuffer(JausTime input, unsigned char *buffer, unsig
 {
 	// Unpack TimeStamp
 	if(!jausUnsignedIntegerFromBuffer(&input->timeStamp, buffer, bufferSizeBytes)) return JAUS_FALSE;
-		
+
 	// Calculate Members from TimeStamp
 	input->millisec = (input->timeStamp >> JAUS_TIME_STAMP_MILLISEC_SHIFT) & JAUS_TIME_STAMP_MILLISEC_MASK;
 	input->second = (input->timeStamp >> JAUS_TIME_STAMP_SECOND_SHIFT) & JAUS_TIME_STAMP_SECOND_MASK;
 	input->minute = (input->timeStamp >> JAUS_TIME_STAMP_MINUTE_SHIFT) & JAUS_TIME_STAMP_MINUTE_MASK;
 	input->hour = (input->timeStamp >> JAUS_TIME_STAMP_HOUR_SHIFT) & JAUS_TIME_STAMP_HOUR_MASK;
 	input->day = (input->timeStamp >> JAUS_TIME_STAMP_DAY_SHIFT) & JAUS_TIME_STAMP_DAY_MASK;
-	
+
 	return JAUS_TRUE;
 }
 
@@ -199,7 +219,7 @@ JausBoolean jausDateStampFromBuffer(JausTime input, unsigned char *buffer, unsig
 {
 	// Unpack DateStamp
 	if(!jausUnsignedShortFromBuffer(&input->dateStamp, buffer, bufferSizeBytes)) return JAUS_FALSE;
-		
+
 	// Calculate Members from DateStamp
 	input->day = (input->dateStamp >> JAUS_DATE_STAMP_DAY_SHIFT) & JAUS_DATE_STAMP_DAY_MASK;
 	input->month = (input->dateStamp >> JAUS_DATE_STAMP_MONTH_SHIFT) & JAUS_DATE_STAMP_MONTH_MASK;
@@ -212,10 +232,10 @@ JausBoolean jausTimeStampToBuffer(JausTime input, unsigned char *buffer, unsigne
 {
 	// Calculate TimeStamp
 	calculateTimeStamp(input);
-	
+
 	// Pack TimeStamp
 	if(!jausUnsignedIntegerToBuffer(input->timeStamp, buffer, bufferSizeBytes)) return JAUS_FALSE;
-	
+
 	return JAUS_TRUE;
 }
 
@@ -223,10 +243,10 @@ JausBoolean jausDateStampToBuffer(JausTime input, unsigned char *buffer, unsigne
 {
 	// Calculate DateStamp
 	calculateDateStamp(input);
-	
+
 	// Pack TimeStamp
 	if(!jausUnsignedShortToBuffer(input->dateStamp, buffer, bufferSizeBytes)) return JAUS_FALSE;
-	
+
 	return JAUS_TRUE;
 }
 
@@ -234,15 +254,15 @@ JausUnsignedInteger jausTimeGetTimeStamp(JausTime time)
 {
 	// Calculate TimeStamp
 	calculateTimeStamp(time);
-	
-	return time->timeStamp;	
+
+	return time->timeStamp;
 }
 
 JausUnsignedShort jausTimeGetDateStamp(JausTime time)
 {
 	// Calculate DateStamp
 	calculateDateStamp(time);
-	
+
 	return time->dateStamp;
 }
 
@@ -265,6 +285,6 @@ JausBoolean calculateDateStamp(JausTime time)
 	time->dateStamp |= (time->day & JAUS_DATE_STAMP_DAY_MASK) << JAUS_DATE_STAMP_DAY_SHIFT;
 	time->dateStamp |= (time->month & JAUS_DATE_STAMP_MONTH_MASK) << JAUS_DATE_STAMP_MONTH_SHIFT;
 	time->dateStamp |= (time->year & JAUS_DATE_STAMP_YEAR_MASK) << JAUS_DATE_STAMP_YEAR_SHIFT;
-	
+
 	return JAUS_TRUE;
 }
