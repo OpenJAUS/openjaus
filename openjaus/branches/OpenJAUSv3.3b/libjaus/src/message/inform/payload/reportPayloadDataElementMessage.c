0c0b5e20db255835c763365dc5e561d45a8d61cd
galluzzt <galluzzt@localhost>
2009-08-26 14:37:37 +0000

    Fixed ticket #120

--- openjaus/branches/OpenJAUSv3.3b/libjaus/src/message/inform/payload/reportPayloadDataElementMessage.c
+++ openjaus/branches/OpenJAUSv3.3b/libjaus/src/message/inform/payload/reportPayloadDataElementMessage.c
@@ -1,12 +1,12 @@
 /*****************************************************************************
  *  Copyright (c) 2008, University of Florida
  *  All rights reserved.
- *  
- *  This file is part of OpenJAUS.  OpenJAUS is distributed under the BSD 
+ *
+ *  This file is part of OpenJAUS.  OpenJAUS is distributed under the BSD
  *  license.  See the LICENSE file for details.
- * 
- *  Redistribution and use in source and binary forms, with or without 
- *  modification, are permitted provided that the following conditions 
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
  *  are met:
  *
  *     * Redistributions of source code must retain the above copyright
@@ -15,20 +15,20 @@
  *       copyright notice, this list of conditions and the following
  *       disclaimer in the documentation and/or other materials provided
  *       with the distribution.
- *     * Neither the name of the University of Florida nor the names of its 
- *       contributors may be used to endorse or promote products derived from 
+ *     * Neither the name of the University of Florida nor the names of its
+ *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
  *
- *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
- *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
- *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
  *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
- *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ****************************************************************************/
 // File Name: reportPayloadDataElementMessage.c
@@ -75,7 +75,11 @@ static void dataInitialize(ReportPayloadDataElementMessage message)
 static void dataDestroy(ReportPayloadDataElementMessage message)
 {
 	// Free message fields
-	if(!message->indexes) free(message->indexes);
+	if(message->indexes)
+	{
+		free(message->indexes);
+		message->indexes = NULL;
+	}
 }
 
 // Return boolean of success
@@ -85,7 +89,7 @@ static JausBoolean dataFromBuffer(ReportPayloadDataElementMessage message, unsig
 	JausByte payloadInformationDataElementCount = 0;
 	JausByte informationInterfaceNumber;
 	JausInformationInterface informationInterface = NULL;
-	
+
 	if(bufferSizeBytes == message->dataSize)
 	{
 		// Unpack Message Fields from Buffer
@@ -95,18 +99,18 @@ static JausBoolean dataFromBuffer(ReportPayloadDataElementMessage message, unsig
 
 		message->numberInterfaces = payloadInformationDataElementCount;
 		message->indexes = NULL;
-		
+
 		if(!payloadInformationDataElementCount) return JAUS_TRUE; // nothing to do
 
 		message->indexes = malloc(payloadInformationDataElementCount * sizeof(int));
-		
+
 		for(i = 0; i < payloadInformationDataElementCount; i++)
-		{			
+		{
 			// unpack payloadDataElement Index number (indexing starts at 1)
 			if(!jausByteFromBuffer(&informationInterfaceNumber, buffer+index, bufferSizeBytes-index)) return JAUS_FALSE;
 			index += JAUS_BYTE_SIZE_BYTES;
 			message->indexes[i] = informationInterfaceNumber;
-			
+
 			// get information interface for the current #
 			if(message->jausPayloadInterface && (message->jausPayloadInterface->jausInformationInterfaces->elementCount >= informationInterfaceNumber) )
 			{
@@ -115,7 +119,7 @@ static JausBoolean dataFromBuffer(ReportPayloadDataElementMessage message, unsig
 
 			// unpack and set payloadDataElement's value
 			if(!jausInformationValueFromBuffer(informationInterface, buffer+index, bufferSizeBytes-index)) return JAUS_FALSE;
-			index += jausInformationValueSizeBytes(informationInterface); 
+			index += jausInformationValueSizeBytes(informationInterface);
 		}
 		return JAUS_TRUE;
 	}
@@ -139,12 +143,12 @@ static int dataToBuffer(ReportPayloadDataElementMessage message, unsigned char *
 		// Pack Message Fields to Buffer
 		// # payload interfaces
 		payloadInformationDataElementCount = message->numberInterfaces;
-		
+
 		if(!jausByteToBuffer(payloadInformationDataElementCount, buffer+index, bufferSizeBytes-index)) return JAUS_FALSE;
 		index += JAUS_BYTE_SIZE_BYTES;
-		
+
 		for(i = 0; i < payloadInformationDataElementCount; i++)
-		{			
+		{
 			// get payloadDataElement identifier
 
 			// get/pack payloadDataElement index
@@ -155,10 +159,10 @@ static int dataToBuffer(ReportPayloadDataElementMessage message, unsigned char *
 				index += JAUS_BYTE_SIZE_BYTES;
 			}
 			else return JAUS_FALSE;
-			
+
 			// get corresponding informationInterface
-			informationInterface = (JausInformationInterface) message->jausPayloadInterface->jausInformationInterfaces->elementData[informationInterfaceNumber - 1];			
-			// get/pack payloadDataElement's value			
+			informationInterface = (JausInformationInterface) message->jausPayloadInterface->jausInformationInterfaces->elementData[informationInterfaceNumber - 1];
+			// get/pack payloadDataElement's value
 			if(!jausInformationValueToBuffer(informationInterface, buffer+index, bufferSizeBytes-index)) return JAUS_FALSE;
 			index += jausInformationValueSizeBytes(informationInterface);
 		}
@@ -178,11 +182,11 @@ static unsigned int dataSize(ReportPayloadDataElementMessage message)
 	// Pack Message Fields to Buffer
 	// # payload interfaces
 	payloadInformationDataElementCount = message->numberInterfaces;
-	
+
 	index += JAUS_BYTE_SIZE_BYTES;
-	
+
 	for(i = 0; i < payloadInformationDataElementCount; i++)
-	{			
+	{
 		// get payloadDataElement identifier
 
 		// get/pack payloadDataElement index
@@ -192,9 +196,9 @@ static unsigned int dataSize(ReportPayloadDataElementMessage message)
 			index += JAUS_BYTE_SIZE_BYTES;
 		}
 		else return 0;
-		
+
 		// get corresponding informationInterface
-		informationInterface = (JausInformationInterface) message->jausPayloadInterface->jausInformationInterfaces->elementData[informationInterfaceNumber - 1];			
+		informationInterface = (JausInformationInterface) message->jausPayloadInterface->jausInformationInterfaces->elementData[informationInterfaceNumber - 1];
 		index += jausInformationValueSizeBytes(informationInterface);
 	}
 
@@ -214,7 +218,7 @@ ReportPayloadDataElementMessage reportPayloadDataElementMessageCreate(void)
 	{
 		return NULL;
 	}
-	
+
 	// Initialize Values
 	message->properties.priority = JAUS_DEFAULT_PRIORITY;
 	message->properties.ackNak = JAUS_ACK_NAK_NOT_REQUIRED;
@@ -228,11 +232,11 @@ ReportPayloadDataElementMessage reportPayloadDataElementMessageCreate(void)
 	message->dataFlag = JAUS_SINGLE_DATA_PACKET;
 	message->dataSize = maxDataSizeBytes;
 	message->sequenceNumber = 0;
-	
+
 	dataInitialize(message);
 	message->dataSize = dataSize(message);
-	
-	return message;	
+
+	return message;
 }
 
 void reportPayloadDataElementMessageDestroy(ReportPayloadDataElementMessage message)
@@ -246,7 +250,7 @@ void reportPayloadDataElementMessageDestroy(ReportPayloadDataElementMessage mess
 JausBoolean reportPayloadDataElementMessageFromBuffer(ReportPayloadDataElementMessage message, unsigned char* buffer, unsigned int bufferSizeBytes)
 {
 	int index = 0;
-	
+
 	if(headerFromBuffer(message, buffer+index, bufferSizeBytes-index))
 	{
 		index += JAUS_HEADER_SIZE_BYTES;
@@ -269,10 +273,10 @@ JausBoolean reportPayloadDataElementMessageToBuffer(ReportPayloadDataElementMess
 {
 	if(bufferSizeBytes < reportPayloadDataElementMessageSize(message))
 	{
-		return JAUS_FALSE; //improper size	
+		return JAUS_FALSE; //improper size
 	}
 	else
-	{	
+	{
 		if(headerToBuffer(message, buffer, bufferSizeBytes))
 		{
 			message->dataSize = dataToBuffer(message, buffer+JAUS_HEADER_SIZE_BYTES, bufferSizeBytes - JAUS_HEADER_SIZE_BYTES);
@@ -288,7 +292,7 @@ JausBoolean reportPayloadDataElementMessageToBuffer(ReportPayloadDataElementMess
 ReportPayloadDataElementMessage reportPayloadDataElementMessageFromJausMessage(JausMessage jausMessage)
 {
 	ReportPayloadDataElementMessage message;
-	
+
 	if(jausMessage->commandCode != commandCode)
 	{
 		return NULL; // Wrong message type
@@ -300,7 +304,7 @@ ReportPayloadDataElementMessage reportPayloadDataElementMessageFromJausMessage(J
 		{
 			return NULL;
 		}
-		
+
 		message->properties.priority = jausMessage->properties.priority;
 		message->properties.ackNak = jausMessage->properties.ackNak;
 		message->properties.scFlag = jausMessage->properties.scFlag;
@@ -315,7 +319,7 @@ ReportPayloadDataElementMessage reportPayloadDataElementMessageFromJausMessage(J
 		message->dataSize = jausMessage->dataSize;
 		message->dataFlag = jausMessage->dataFlag;
 		message->sequenceNumber = jausMessage->sequenceNumber;
-		
+
 		// Unpack jausMessage->data
 		if(dataFromBuffer(message, jausMessage->data, jausMessage->dataSize))
 		{
@@ -331,13 +335,13 @@ ReportPayloadDataElementMessage reportPayloadDataElementMessageFromJausMessage(J
 JausMessage reportPayloadDataElementMessageToJausMessage(ReportPayloadDataElementMessage message)
 {
 	JausMessage jausMessage;
-	
+
 	jausMessage = (JausMessage)malloc( sizeof(struct JausMessageStruct) );
 	if(jausMessage == NULL)
 	{
 		return NULL;
-	}	
-	
+	}
+
 	jausMessage->properties.priority = message->properties.priority;
 	jausMessage->properties.ackNak = message->properties.ackNak;
 	jausMessage->properties.scFlag = message->properties.scFlag;
@@ -352,10 +356,10 @@ JausMessage reportPayloadDataElementMessageToJausMessage(ReportPayloadDataElemen
 	jausMessage->dataSize = dataSize(message);
 	jausMessage->dataFlag = message->dataFlag;
 	jausMessage->sequenceNumber = message->sequenceNumber;
-	
-	jausMessage->data = (unsigned char *)malloc(jausMessage->dataSize);	
+
+	jausMessage->data = (unsigned char *)malloc(jausMessage->dataSize);
 	jausMessage->dataSize = dataToBuffer(message, jausMessage->data, jausMessage->dataSize);
-		
+
 	return jausMessage;
 }
 
@@ -381,25 +385,25 @@ static JausBoolean headerFromBuffer(ReportPayloadDataElementMessage message, uns
 		message->properties.expFlag	 = ((buffer[0] >> 7) & 0x01);
 		message->properties.version	 = (buffer[1] & 0x3F);
 		message->properties.reserved = ((buffer[1] >> 6) & 0x03);
-		
+
 		message->commandCode = buffer[2] + (buffer[3] << 8);
-	
+
 		message->destination->instance = buffer[4];
 		message->destination->component = buffer[5];
 		message->destination->node = buffer[6];
 		message->destination->subsystem = buffer[7];
-	
+
 		message->source->instance = buffer[8];
 		message->source->component = buffer[9];
 		message->source->node = buffer[10];
 		message->source->subsystem = buffer[11];
-		
+
 		message->dataSize = buffer[12] + ((buffer[13] & 0x0F) << 8);
 
 		message->dataFlag = ((buffer[13] >> 4) & 0x0F);
 
 		message->sequenceNumber = buffer[14] + (buffer[15] << 8);
-		
+
 		return JAUS_TRUE;
 	}
 }
@@ -407,13 +411,13 @@ static JausBoolean headerFromBuffer(ReportPayloadDataElementMessage message, uns
 static JausBoolean headerToBuffer(ReportPayloadDataElementMessage message, unsigned char *buffer, unsigned int bufferSizeBytes)
 {
 	JausUnsignedShort *propertiesPtr = (JausUnsignedShort*)&message->properties;
-	
+
 	if(bufferSizeBytes < JAUS_HEADER_SIZE_BYTES)
 	{
 		return JAUS_FALSE;
 	}
 	else
-	{	
+	{
 		buffer[0] = (unsigned char)(*propertiesPtr & 0xFF);
 		buffer[1] = (unsigned char)((*propertiesPtr & 0xFF00) >> 8);
 
@@ -429,13 +433,13 @@ static JausBoolean headerToBuffer(ReportPayloadDataElementMessage message, unsig
 		buffer[9] = (unsigned char)(message->source->component & 0xFF);
 		buffer[10] = (unsigned char)(message->source->node & 0xFF);
 		buffer[11] = (unsigned char)(message->source->subsystem & 0xFF);
-		
+
 		buffer[12] = (unsigned char)(message->dataSize & 0xFF);
 		buffer[13] = (unsigned char)((message->dataFlag & 0xFF) << 4) | (unsigned char)((message->dataSize & 0x0F00) >> 8);
 
 		buffer[14] = (unsigned char)(message->sequenceNumber & 0xFF);
 		buffer[15] = (unsigned char)((message->sequenceNumber & 0xFF00) >> 8);
-		
+
 		return JAUS_TRUE;
 	}
 }
