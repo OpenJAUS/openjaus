b67cd25889328b0393491696742a1a3278c6e7a0
galluzzt <galluzzt@localhost>
2009-08-27 00:18:56 +0000

    Added fix for ticket #102

--- openjaus/branches/OpenJAUSv3.3b/libopenJaus/src/utils/multicastSocket.c
+++ openjaus/branches/OpenJAUSv3.3b/libopenJaus/src/utils/multicastSocket.c
@@ -1,12 +1,12 @@
 /*****************************************************************************
  *  Copyright (c) 2008, University of Florida
  *  All rights reserved.
- *  
- *  This file is part of OpenJAUS.  OpenJAUS is distributed under the BSD 
+ *
+ *  This file is part of OpenJAUS.  OpenJAUS is distributed under the BSD
  *  license.  See the LICENSE file for details.
- * 
- *  Redistribution and use in source and binary forms, with or without 
- *  modification, are permitted provided that the following conditions 
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
  *  are met:
  *
  *     * Redistributions of source code must retain the above copyright
@@ -15,20 +15,20 @@
  *       copyright notice, this list of conditions and the following
  *       disclaimer in the documentation and/or other materials provided
  *       with the distribution.
- *     * Neither the name of the University of Florida nor the names of its 
- *       contributors may be used to endorse or promote products derived from 
+ *     * Neither the name of the University of Florida nor the names of its
+ *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
  *
- *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
- *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
- *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
  *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
- *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ****************************************************************************/
 // File Name: multicastSocket.c
@@ -52,8 +52,8 @@ MulticastSocket multicastSocketCreate(short port, InetAddress ipAddress)
 	MulticastSocket multicastSocket;
 	struct sockaddr_in address;
 	socklen_t addressLength = sizeof(address);
-		
-#ifdef WIN32	
+
+#ifdef WIN32
 	// Initialize the socket subsystem
 	WSADATA wsaData;
 	int err;
@@ -72,9 +72,9 @@ MulticastSocket multicastSocketCreate(short port, InetAddress ipAddress)
 	multicastSocket->address = inetAddressCreate();
 	multicastSocket->unicastSocketDescriptor = -1;
 	multicastSocket->multicastSocketDescriptor = -1;
-	
+
 	// Open a socket with: Protocol Family (PF) IPv4, of Datagram Socket Type, and using UDP IP protocol explicitly
-	multicastSocket->unicastSocketDescriptor = (int) socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP); 
+	multicastSocket->unicastSocketDescriptor = (int) socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	if(multicastSocket->unicastSocketDescriptor  == -1)
 	{
 		multicastSocketDestroy(multicastSocket);
@@ -85,7 +85,7 @@ MulticastSocket multicastSocketCreate(short port, InetAddress ipAddress)
 	address.sin_family = AF_INET;					// Set Internet Socket (sin), Family to: Address Family (AF) IPv4 (INET)
 	address.sin_addr.s_addr = ipAddress->value;		// Set Internet Socket (sin), Address to: The ipAddressString argument
 	address.sin_port = htons(port);					// Set Internet Socket (sin), Port to: the port argument
-	
+
 	// Bind our open socket to a free port on the given interface, with our defined ipAddress
 	if(bind(multicastSocket->unicastSocketDescriptor, (struct sockaddr *)&address, sizeof(address)))
 	{
@@ -113,7 +113,11 @@ MulticastSocket multicastSocketCreate(short port, InetAddress ipAddress)
 	multicastSocket->timeout.tv_usec = 0;
 	multicastSocket->blocking = 1;
 	multicastSocket->multicastSocketDescriptor = -1;
-	
+
+	// Initialize parameters needed to select on socket to read in data
+	multicastSocket->maxDescriptor = multicastSocket->unicastSocketDescriptor;
+	FD_ZERO(&multicastSocket->readSet);
+
 	return multicastSocket;
 }
 
@@ -123,16 +127,16 @@ void multicastSocketDestroy(MulticastSocket multicastSocket)
 	{
 		CLOSE_SOCKET(multicastSocket->unicastSocketDescriptor);
 	}
-	
+
 	if(multicastSocket->multicastSocketDescriptor != -1)
 	{
 		CLOSE_SOCKET(multicastSocket->multicastSocketDescriptor);
 	}
-	
-	inetAddressDestroy(multicastSocket->address);	
+
+	inetAddressDestroy(multicastSocket->address);
 	free(multicastSocket);
 
-#ifdef WIN32	
+#ifdef WIN32
 	// Initialize the socket subsystem
 	WSACleanup();
 #endif
@@ -142,7 +146,7 @@ void multicastSocketDestroy(MulticastSocket multicastSocket)
 int multicastSocketJoinGroup(MulticastSocket multicastSocket, InetAddress groupIpAddress)
 {
 	struct ip_mreq multicastRequest;
-	
+
 	if(multicastSocket == NULL)
 	{
 		return -1;
@@ -158,7 +162,7 @@ int multicastSocketJoinGroup(MulticastSocket multicastSocket, InetAddress groupI
 
 #if defined(__linux) || defined(linux) || defined(__linux__) || defined(__APPLE__)
 	// Open a socket with: Protocol Family (PF) IPv4, of Datagram Socket Type, and using UDP IP protocol explicitly
-	multicastSocket->multicastSocketDescriptor = (int) socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP); 
+	multicastSocket->multicastSocketDescriptor = (int) socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	if(multicastSocket->multicastSocketDescriptor  == -1)
 	{
 		return -1;
@@ -177,10 +181,16 @@ int multicastSocketJoinGroup(MulticastSocket multicastSocket, InetAddress groupI
 		CLOSE_SOCKET(multicastSocket->multicastSocketDescriptor);
 		return -1;
 	}
-	
-	return setsockopt(multicastSocket->multicastSocketDescriptor, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&multicastRequest, sizeof(multicastRequest)); 
+
+	// Set max descriptor parameter needed to select on socket during read operation
+	if(multicastSocket->multicastSocketDescriptor > multicastSocket->maxDescriptor)
+	{
+		multicastSocket->maxDescriptor = multicastSocket->multicastSocketDescriptor;
+	}
+
+	return setsockopt(multicastSocket->multicastSocketDescriptor, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&multicastRequest, sizeof(multicastRequest));
 #else
-	return setsockopt(multicastSocket->unicastSocketDescriptor, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&multicastRequest, sizeof(multicastRequest)); 
+	return setsockopt(multicastSocket->unicastSocketDescriptor, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&multicastRequest, sizeof(multicastRequest));
 #endif
 
 }
@@ -194,7 +204,7 @@ int multicastSocketSend(MulticastSocket multicastSocket, DatagramPacket packet)
 	toAddress.sin_family = AF_INET;							// Set Internet Socket (sin), Family to: Address Family (AF) IPv4 (INET)
 	toAddress.sin_addr.s_addr = packet->address->value;		// Set Internet Socket (sin), Address to: The packet ipAddressString
 	toAddress.sin_port = htons(packet->port);				// Set Internet Socket (sin), Port to: the packet port
-	
+
 	bytesSent = sendto(multicastSocket->unicastSocketDescriptor, (void *)packet->buffer, packet->bufferSizeBytes, 0, (struct sockaddr *)&toAddress, sizeof(toAddress));
 
 	return bytesSent;
@@ -204,50 +214,58 @@ int multicastSocketReceive(MulticastSocket multicastSocket, DatagramPacket packe
 {
 	struct timeval timeout;
 	struct timeval *timeoutPtr = NULL;
-	fd_set readSet;
 	struct sockaddr_in fromAddress;
-	socklen_t fromAddressLength;
+	socklen_t fromAddressLength = sizeof(fromAddress);
 	int bytesReceived = 0;
 	int count = 0;
-	int socket = 0;
-	
-	memset(&fromAddress, 0, sizeof(fromAddress));
-	
-	if(!multicastSocket->blocking)
-	{
-		timeout = multicastSocket->timeout;
-		timeoutPtr = &timeout;
-	}
 
-	FD_ZERO(&readSet);
-	FD_SET(multicastSocket->unicastSocketDescriptor, &readSet);
-	socket = multicastSocket->unicastSocketDescriptor;
-	
-	if(multicastSocket->multicastSocketDescriptor != -1)
+	// If no file descriptors are ready for reading
+	if(	!FD_ISSET(multicastSocket->unicastSocketDescriptor, &multicastSocket->readSet) &&
+		(multicastSocket->multicastSocketDescriptor == -1 || !FD_ISSET(multicastSocket->multicastSocketDescriptor, &multicastSocket->readSet)))
 	{
-		FD_SET(multicastSocket->multicastSocketDescriptor, &readSet);
-		socket = multicastSocket->multicastSocketDescriptor;
-	}
+		// Reset readSet just in case any other bits are set
+		FD_ZERO(&multicastSocket->readSet);
 
-	count = select(socket + 1, &readSet, NULL, NULL, timeoutPtr);
-	if(count > 0)
-	{
-		fromAddressLength = sizeof(fromAddress);
+		// Setup readSet to instruct select to listen to the unicast descriptor
+		FD_SET(multicastSocket->unicastSocketDescriptor, &multicastSocket->readSet);
 
-		if(FD_ISSET(multicastSocket->unicastSocketDescriptor, &readSet))
+		if(multicastSocket->multicastSocketDescriptor != -1)
 		{
-			bytesReceived = recvfrom(multicastSocket->unicastSocketDescriptor, packet->buffer, packet->bufferSizeBytes, 0, (struct sockaddr*)&fromAddress, &fromAddressLength);
+			// Setup readSet to instruct select to listen to the multicast descriptor
+			FD_SET(multicastSocket->multicastSocketDescriptor, &multicastSocket->readSet);
 		}
-		if(multicastSocket->multicastSocketDescriptor != -1 && FD_ISSET(multicastSocket->multicastSocketDescriptor, &readSet))
+
+		// Setup timeout for select
+		if(!multicastSocket->blocking)
 		{
-			bytesReceived = recvfrom(multicastSocket->multicastSocketDescriptor, packet->buffer, packet->bufferSizeBytes, 0, (struct sockaddr*)&fromAddress, &fromAddressLength);
+			timeout = multicastSocket->timeout;
+			timeoutPtr = &timeout;
+		}
+
+		count = select(multicastSocket->maxDescriptor + 1, &multicastSocket->readSet, NULL, NULL, timeoutPtr);
+		if(count <= 0) // Either something went wrong or the timeout expired
+		{
+			// Reset readSet just in case any bits are set
+			FD_ZERO(&multicastSocket->readSet);
+			return -1;
 		}
 	}
-	else
+
+	// Zero from address
+	memset(&fromAddress, 0, fromAddressLength);
+
+	// Check to see which socket is ready for reading and read that socket one at a time
+	if(FD_ISSET(multicastSocket->unicastSocketDescriptor, &multicastSocket->readSet))
 	{
-		return -1;
+		FD_CLR(multicastSocket->unicastSocketDescriptor, &multicastSocket->readSet);
+		bytesReceived = recvfrom(multicastSocket->unicastSocketDescriptor, packet->buffer, packet->bufferSizeBytes, 0, (struct sockaddr*)&fromAddress, &fromAddressLength);
 	}
-	
+	else if(FD_ISSET(multicastSocket->multicastSocketDescriptor, &multicastSocket->readSet))
+	{
+		FD_CLR(multicastSocket->unicastSocketDescriptor, &multicastSocket->readSet);
+		bytesReceived = recvfrom(multicastSocket->unicastSocketDescriptor, packet->buffer, packet->bufferSizeBytes, 0, (struct sockaddr*)&fromAddress, &fromAddressLength);
+	}
+
 	if(bytesReceived != -1)
 	{
 		packet->port = ntohs(fromAddress.sin_port);
@@ -263,10 +281,10 @@ int multicastSocketReceive(MulticastSocket multicastSocket, DatagramPacket packe
 void multicastSocketSetTimeout(MulticastSocket multicastSocket, double timeoutSec)
 {
 	long sec, usec;
-	
+
 	sec = (long)timeoutSec;
 	usec = (long)((timeoutSec - (double)sec) * 1.0e6);
-			
+
 	multicastSocket->timeout.tv_sec = sec;
 	multicastSocket->timeout.tv_usec = usec;
 	multicastSocket->blocking = 0;
